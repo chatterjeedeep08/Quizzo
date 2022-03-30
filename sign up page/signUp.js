9b8ec44de4e0b2fc88eaa814387940e203637789
annkiaa05 <annika.chandra2000@gmail.com>
2022-03-30 20:40:56 +0530

    updated

--- sign up page/signUp.js
+++ sign up page/signUp.js
@@ -1,3 +1,21 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyBA65SYSXms26oFfZ1SoenAJv5AmSn6_RQ",
+	authDomain: "quizzo-716e1.firebaseapp.com",
+	projectId: "quizzo-716e1",
+	storageBucket: "quizzo-716e1.appspot.com",
+	messagingSenderId: "889297898254",
+	appId: "1:889297898254:web:e2d230fabe20a0a695a55f"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  //const app = initializeApp(firebaseConfig);
+
+const auth =firebase.auth();
+const database =firebase.database();
+
+
 const form = document.querySelector('.form-container form');
 const inputs = document.querySelectorAll('.form-container input');
 
@@ -22,5 +40,21 @@ form.addEventListener('submit', (e) => {
 
 function validateEmail (email) {
 	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	alert(re.test(String(email).toLowerCase()));
+
 	return re.test(String(email).toLowerCase());
+}
+function validatePassword(password){
+	if(password <6){
+		return false;
+	}
+	else{
+		return true;
+	}
+}
+function signUp(){
+	first_name= document.getElementById('first-name').value;
+	last_name= document.getElementById('last-name').value;
+	email= document.getElementById('email').value;
+	password= document.getElementById('password').value;
 }
